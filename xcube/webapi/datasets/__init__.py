75fbbbb7a4232f0d92b05d2e79d4912f5632455f
Loveday <ben.loveday@external.eumetsat.int>
2024-12-02 10:12:16 +0100

    updates

---
+++ xcube/webapi/datasets/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2018-2024 by xcube team and contributors
+# Permissions are hereby granted under the terms of the MIT License:
+# https://opensource.org/licenses/MIT.
+
+from .routes import PATH_PARAM_DATASET_ID
+from .routes import PATH_PARAM_VAR_NAME
+from .routes import QUERY_PARAM_CBAR
+from .routes import QUERY_PARAM_CRS
+from .routes import QUERY_PARAM_VMAX
+from .routes import QUERY_PARAM_VMIN
+from .routes import api
